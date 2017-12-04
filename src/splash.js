e1b605426d08b6eaa2867a69859416fd8f9bd0f8
Ethan Han <ethan.han47@gmail.com>
2017-12-04 17:58:08 -0500

    adding handlers for settings selection and room id input

--- src/splash.js
+++ src/splash.js
@@ -11,8 +11,40 @@ class Splash extends Component {
     super(props);
 
     this.state = {
-      inputRoomId: null
+      inputRoomId: '',
+      deadPool: false,
+      turnTimer: false,
+      involvedOnly: false
     }
+
+    this.handleInputRoomId = this.handleInputRoomId.bind(this);
+    this.toggleDeadPool = this.toggleDeadPool.bind(this);
+    this.toggleTurnTimer = this.toggleTurnTimer.bind(this);
+    this.toggleInvolvedOnly = this.toggleInvolvedOnly.bind(this);
+  }
+
+  toggleDeadPool(event) {
+    this.setState({
+      deadPool: !this.state.deadPool
+    });
+  }
+
+  toggleTurnTimer(event) {
+    this.setState({
+      turnTimer: !this.state.turnTimer
+    });
+  }
+
+  toggleInvolvedOnly(event) {
+    this.setState({
+      involvedOnly: !this.state.involvedOnly
+    });
+  }
+
+  handleInputRoomId(event) {
+    this.setState({
+      inputRoomId: event.target.value
+    });
   }
 
   render() {
@@ -25,9 +57,27 @@ class Splash extends Component {
             <div className="create">
               <h4 className="header">Create New Room</h4>
               <ul>
-                <li><input id="1" type="checkbox"/><label htmlFor="1">Dead Pool</label></li>
-                <li><input id="2" type="checkbox"/><label htmlFor="2">Turn Timer</label></li>
-                <li><input id="3" type="checkbox"/><label htmlFor="3">Involved Only</label></li>
+                <li>
+                  <input
+                    onChange={this.toggleDeadPool}
+                    value={this.state.deadPool}
+                    id="1"
+                    type="checkbox"/>
+                  <label htmlFor="1">Dead Pool</label></li>
+                <li>
+                  <input
+                    onChange={this.toggleTurnTimer}
+                    value={this.state.turnTimer}
+                    id="2"
+                    type="checkbox"/>
+                  <label htmlFor="2">Turn Timer</label></li>
+                <li>
+                  <input
+                    onChange={this.toggleInvolvedOnly}
+                    value={this.state.involvedOnly}
+                    id="3"
+                    type="checkbox"/>
+                  <label htmlFor="3">Involved Only</label></li>
               </ul>
               <div className="new-room">START</div>
             </div>
@@ -39,7 +89,13 @@ class Splash extends Component {
           <div className="right">
             <div className="join">
             <h4 className="header">Join A Room</h4>
-              <input className="input-room-id" placeholder="ID#" type="text" pattern="[0-9]*" />
+              <input
+                value={this.state.inputRoomId}
+                onChange={this.handleInputRoomId}
+                className="input-room-id"
+                placeholder="ID#"
+                type="text"
+                pattern="[0-9]*" />
               <div className="join-room">JOIN</div>
             </div>
           </div>
